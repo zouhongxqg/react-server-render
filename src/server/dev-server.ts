9d46e43de6cb33748d80a2bc7e342d67e364e59c
zouhong <zouhong@meituan.com>
2018-12-17 20:11:32 +0800

    update package.json

--- src/server/dev-server.ts
+++ src/server/dev-server.ts
@@ -8,7 +8,6 @@ const root = config.get('root') as string;
 const webpackConfig = require(path.resolve(root, './config/webpack.dev.js'));
 const compiler = webpack(webpackConfig);
 
-// @thx shiyun 4 hot load
 const server = new WebpackDevServer(compiler, {
   headers: { 'Access-Control-Allow-Origin': '*' },
   publicPath: webpackConfig.output.publicPath,
